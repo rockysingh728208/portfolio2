d5f63e16119b598e7b736c6d83741cc207a06eaf
rockysingh728208 <sagarkashyap9155@example.com>
2025-08-26 18:37:02 +0530

    jrejer

--- my-project/src/pages/About.jsx
+++ my-project/src/pages/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
 
   return (
     <div className="bg-gray-900 text-white">
-      <div className="min-h-screen flex flex-col items-center px-4 pt-12 md:pt-20">
+      <div className="min-h-screen flex flex-col items-center px-4 pt-16 md:pt-20">
         {/* Heading */}
         <h1 className="text-4xl md:text-5xl font-bold mb-6">About Me</h1>
 
